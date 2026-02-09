40179ab85d14b9283529fa4f6896b1856a7ab2ef
Coll0 <mwandikwacollins@gmail.com>
2026-02-09 13:44:18 +0300

    Still haven't fixed server

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import { connectDB } from "./src/config/database.ts";
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is up and running on port: ${PORT}`);
+    }
+    );
+});
---
+++ backend/src/config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+   try {
+     await mongoose.connect(process.env.MONGODB_URI! as string);
+     console.log('✅MongoDB connected successfully');
+   } catch (error){
+        console.error('MongoDB connection error:', error);
+        process.exit(1); 
+}
+};
