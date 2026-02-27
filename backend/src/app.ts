7cd5b9af8e82925c1704fa74522081d468574e92
Coll0 <mwandikwacollins@gmail.com>
2026-02-27 23:16:44 +0300

    Still haven't fixed the MongoDB error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,4 +8,10 @@ app.get('/health', (req, res) => {
     res.json({ status: "ok", message : "Server is running" });
 });
 
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 export default app;
